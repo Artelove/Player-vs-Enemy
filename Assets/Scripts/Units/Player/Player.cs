56ca5eb5844e190ede802cb81a53e22f0e4cf594
Artelove <arteevil.off@gmail.com>
2022-08-08 01:32:00 +0300

    feat(Unit): Parent class for Enemy and Player
    Unit contains Physics components.
    Unit Init the model components with config.

--- Assets/Scripts/Units/Player/Player.cs
+++ Assets/Scripts/Units/Player/Player.cs
@@ -2,25 +2,16 @@ using System;
 using Movement;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Unit
 {
     [SerializeField] private UnitPhysicsConfig _physicsConfig;
-    private MovementView _movementView;
-    private PhysicsModel _physicsModel;
-    private PhysicsInput _input;
-    
     public event Action Destroyed;
-    private void Awake()
+
+    private void Start()
     {
-        _input = GetComponent<PhysicsInput>();
-        _movementView = GetComponent<MovementView>();
-        _input.HorizintalDirectionChanged += _movementView.ChangeVisual;
-        _physicsModel = new PhysicsModel(_physicsConfig, GetComponent<Rigidbody2D>(), _input);
+        Init(_physicsConfig);
     }
-    private void FixedUpdate()
-    {
-        _physicsModel.UpdatePosition(Time.deltaTime);
-    }
+
     private void OnDestroy()
     {
         Destroyed?.Invoke();
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Movement;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    protected MovementView MovementView;
+    protected PhysicsModel PhysicsModel;
+    protected PhysicsInput Input;
+    protected void Init(UnitPhysicsConfig unitPhysicsConfig)
+    {
+        Input = GetComponent<PhysicsInput>();
+        MovementView = GetComponent<MovementView>();
+        Input.HorizintalDirectionChanged += MovementView.ChangeVisual;
+        PhysicsModel = new PhysicsModel(unitPhysicsConfig, GetComponent<Rigidbody2D>(), Input);
+    }
+    private void FixedUpdate()
+    {
+        PhysicsModel.UpdatePosition(Time.deltaTime);
+    }
+}
