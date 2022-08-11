28e4d6cb96094ba5d584c9a18bc48e5967304180
Artelove <arteevil.off@gmail.com>
2022-08-12 00:31:39 +0300

    feat(Game): Dependeses relationships between components. Game Prefab
    add: Simple Prefab Game for fast setting level [SerializeField]
    add: handlers to UI buttons
    add: load\save levelSaveData

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -1,32 +1,55 @@
 ﻿
 using System;
+using Save;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(LevelController))]
 public class Game : MonoBehaviour
 {
+    [SerializeField] private SceneController _sceneController;
+    [SerializeField] private EndLevelUIPresenter _endLevelUI;
     private LevelController _levelController;
-    private SceneController _sceneController;
+    private LevelSaveSystem _levelSaveSystem;
+    private LevelSaveData _levelSaveData;
+    
     private void Awake()
     {
         _levelController = GetComponent<LevelController>();
-        _sceneController = new SceneController();
+    }
+
+    private void Start()
+    {
+        _sceneController.CurrentScene = SceneManager.GetActiveScene();
+        _levelSaveSystem = new LevelSaveSystem(_sceneController.CurrentScene.name);
+        _levelSaveData = _levelSaveSystem.Load();
+        _endLevelUI.View.NextLevel.onClick.AddListener(_sceneController.NextLevel);
+        _endLevelUI.View.RestartLevel.onClick.AddListener(_sceneController.RestartLevel);
+        _endLevelUI.View.LevelMenu.onClick.AddListener(_sceneController.LoadLevelMenu);
     }
 
     private void OnEnable()
     {
-        _levelController.LevelLosed += LosedLevel;
-        _levelController.LevelDone += WinLevel;
+        _levelController.LevelLosed += Lose;
+        _levelController.LevelDone += Win;
     }
 
-    private void LosedLevel()
+    private void Lose()
     {
-        _sceneController.RestartLevel(SceneManager.GetActiveScene().buildIndex);
+        _levelSaveData.AttemptsToWin[^1]++;
+        EndLevel();
     }
 
-    private void WinLevel()
+    private void Win()
     {
-        _sceneController.NextLevel(SceneManager.GetActiveScene().buildIndex);
+        _levelSaveData.IsLevelDone = true;
+        EndLevel();
+    }
+
+    private void EndLevel()
+    {
+        _levelSaveSystem.Save(_levelSaveData);
+        _endLevelUI.Model.SetLevelData(_levelSaveData);
+        _endLevelUI.Show();
     }
 }
